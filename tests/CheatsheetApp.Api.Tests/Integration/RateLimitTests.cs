fd3c1f8ea49ae6d0eafa5521123aa3f615138675
NgTuanLoc <tuanloc2352000@gmail.com>
2026-06-10 12:43:38 +0700

    test: fix magic number and use constant for admin username in RateLimitTests

--- tests/CheatsheetApp.Api.Tests/Integration/RateLimitTests.cs
+++ tests/CheatsheetApp.Api.Tests/Integration/RateLimitTests.cs
@@ -8,13 +8,16 @@ public class RateLimitTests(ApiFixture fixture) : IClassFixture<ApiFixture>
     [Fact]
     public async Task Sixth_login_attempt_within_a_minute_returns_429()
     {
+        // One over the 5/min rate limit configured in Program.cs
+        const int AttemptsOverLimit = 6;
+
         var client = fixture.CreateClient();
         var statuses = new List<HttpStatusCode>();
 
-        for (var i = 0; i < 6; i++)
+        for (var i = 0; i < AttemptsOverLimit; i++)
         {
             var response = await client.PostAsJsonAsync("/auth/login",
-                new { username = "admin", password = "wrong" });
+                new { username = ApiFixture.AdminUsername, password = "wrong" });
             statuses.Add(response.StatusCode);
         }
 
